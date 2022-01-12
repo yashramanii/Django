1e0d6b3789b5ef2f4b1bb0a7789841d12e934fb9
yramanii <ramaniyash19@gmail.com>
2022-01-12 17:38:11 +0530

    addition

--- weather/api/views.py
+++ weather/api/views.py
@@ -41,3 +41,21 @@ class TodoListApiView(ModelViewSet):
     queryset = Todo.objects.all()
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = TodoSerializer
+
+def index(request):
+    return render(request, "index.html")
+
+def addition(request):
+
+    num1 = request.POST['num1']
+    num2 = request.POST['num2']
+
+    if num1.isdigit() and num2.isdigit():
+        a = int(num1)
+        b = int(num2)
+        res = a + b
+
+        return render(request, "result.html", {"result": res})
+    else:
+        res = "Only digits are allowed"
+        return render(request, "result.html", {"result": res})
