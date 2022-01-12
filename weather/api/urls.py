d660e96e8bd04f950ce1cd57114941590e8f79dc
yashramanii <96617148+yashramanii@users.noreply.github.com>
2022-01-12 19:02:31 +0530

    subtraction added (#4) (#5)
    * subtraction added
    
    * added underline
    
    Co-authored-by: yramanii <52834786+yramanii@users.noreply.github.com>

--- weather/api/urls.py
+++ weather/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
-from api.views import TodoListApiView, index, addition
+from api.views import TodoListApiView, index, addition, subtraction
 
 router = routers.DefaultRouter()
 router.register('todo', TodoListApiView)
@@ -11,4 +11,5 @@ urlpatterns = [
     # path('', TodoListApiView.as_view())
     path('',index, name='index'),
     path('add',addition, name='add'),
+    path('sub',addition, name='sub'),
 ]
