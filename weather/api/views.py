51d262ff89575bcf13f02180997df42d0ece8932
yramanii <52834786+yramanii@users.noreply.github.com>
2022-01-13 16:15:30 +0530

    multiplication func added
    * divide func added
    
    * multiplication func added
    
    * Revert "multiplication func added"
    
    This reverts commit 93b39a890f5b5a83ca9a4607f9fe3ff5578e9bfc.
    
    * multiplication func added

--- weather/api/views.py
+++ weather/api/views.py
@@ -90,3 +90,18 @@ def divide(request):
     else:
         res = "Only digits are allowed"
         return render(request, "result.html", {"result": res})
+
+def multiplication(request):
+
+    num1 = request.POST['num1']
+    num2 = request.POST['num2']
+
+    if num1.isdigit() and num2.isdigit():
+        a = int(num1)
+        b = int(num2)
+        res = a * b
+
+        return render(request, "result.html", {"result": res})
+    else:
+        res = "Only digits are allowed"
+        return render(request, "result.html", {"result": res})
