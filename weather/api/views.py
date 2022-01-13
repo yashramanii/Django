481fd4d73e21488fe02e216d2e7aeecf6fe94a13
yramanii <52834786+yramanii@users.noreply.github.com>
2022-01-13 15:55:30 +0530

    divide func added

--- weather/api/views.py
+++ weather/api/views.py
@@ -75,3 +75,18 @@ def subtraction(request):
     else:
         res = "Only digits are allowed"
         return render(request, "result.html", {"result": res})
+
+def divide(request):
+
+    num1 = request.POST['num1']
+    num2 = request.POST['num2']
+
+    if num1.isdigit() and num2.isdigit():
+        a = int(num1)
+        b = int(num2)
+        res = a / b
+
+        return render(request, "result.html", {"result": res})
+    else:
+        res = "Only digits are allowed"
+        return render(request, "result.html", {"result": res})
