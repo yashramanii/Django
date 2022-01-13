e41c9776d3214214a68fd34c7d83a4ab896251c7
yashramanii <96617148+yashramanii@users.noreply.github.com>
2022-01-13 16:18:41 +0530

    All the functions added
    * subtraction added
    
    * added underline
    
    * subtraction added (#4)
    
    * subtraction added
    
    * added underline
    
    * multiplication func added
    
    * Revert "Multiplication functionality added in Feature2" (#7)
    
    * divide func added
    
    * multiplication func added
    
    * divide func added
    
    * multiplication func added
    
    * Revert "multiplication func added"
    
    This reverts commit 93b39a890f5b5a83ca9a4607f9fe3ff5578e9bfc.
    
    * multiplication func added
    
    Co-authored-by: yramanii <ramaniyash19@gmail.com>
    Co-authored-by: yramanii <52834786+yramanii@users.noreply.github.com>

--- weather/api/urls.py
+++ weather/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
-from api.views import TodoListApiView, index, addition, subtraction
+from api.views import TodoListApiView, divide, index, addition, multiplication, subtraction
 
 router = routers.DefaultRouter()
 router.register('todo', TodoListApiView)
@@ -11,5 +11,7 @@ urlpatterns = [
     # path('', TodoListApiView.as_view())
     path('',index, name='index'),
     path('add',addition, name='add'),
-    path('sub',addition, name='sub'),
+    path('sub',subtraction, name='sub'),
+    path('mul',multiplication, name='mul'),
+    path('div',divide, name='div'),
 ]
